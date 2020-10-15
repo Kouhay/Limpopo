94616604a8d91f84cf3a649d8a1e72dd46a1b58f
Kouhay <72698519+Kouhay@users.noreply.github.com>
2020-10-15 02:28:02 +0200

    Update exam.cpp

--- exam/exam.cpp
+++ exam/exam.cpp
@@ -1,43 +1,31 @@
-#include <iostream>
 #include "http_server.h"
 #include <boost/program_options.hpp>
+#include <iostream>
 
-namespace po = boost::program_options;
-
-namespace {
-
-const unsigned kNumberOfThreads = 4;
-
-} // namespace
+namespace theboost = boost::program_options;
 
 int main(int argc, char** argv)
 {
-    po::options_description desc("Available options");
+    theboost::options_description desc("Available options");
     desc.add_options()
-        ("help", "produce help message")
-        ("directory,d", po::value<std::string>(), "root directory of server")
-        ("address,h", po::value<std::string>(), "address to listen on")
-        ("port,p", po::value<std::string>(), "port number")
-        ;
+        ("ip,h", theboost::value<std::string>(), "address to listen on")
+        ("port,p", theboost::value<std::string>(), "port number")
+        ("directory,d", theboost::value<std::string>(), "root directory of server");
 
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
-
-    if (vm.count("help")) {
-        std::cout << desc << "\n";
-        return 0;
-    }
-
-    if (!vm.count("address") || !vm.count("directory") || !vm.count("port")) {
+    theboost::variables_map vm;
+    theboost::store(theboost::parse_command_line(argc, argv, desc), vm);
+    theboost::notify(vm);
+    
+    if (!vm.count("ip") || !vm.count("directory") || !vm.count("port")) {
         std::cerr << "Missed arguments\n" << desc << "\n";
         return 1;
     }
 
-    const auto directory = vm["directory"].as<std::string>();
-    const auto address = vm["address"].as<std::string>();
+    const auto ip = vm["ip"].as<std::string>();
     const auto port = vm["port"].as<std::string>();
+    const auto directory = vm["directory"].as<std::string>();
+    const unsigned fluxnumber = 5;
 
-    HttpServer server{ directory, address, port, kNumberOfThreads };
+    HttpServer server{ ip, port, directory, fluxnumber };
     server.run();
 }
